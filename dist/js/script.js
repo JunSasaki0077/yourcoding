f1cea09fdc779257e3894a2d8015a65b558274a4
Junsasaki0077 <ikasas0077@gmail.com>
2021-10-11 23:52:20 +0900

    クラス名変更

---
+++ dist/js/script.js
@@ -0,0 +1,75 @@
+//ハンバーガーメニュー
+$('.burger-btn').on('click', function () {
+  $('.burger-btn').toggleClass('close');
+  $('.nav-wrapper').fadeToggle(500);
+});
+// 画面外にクリックしたときメニューを閉じる
+let nav = $('.burger-btn');
+$('section').click(function () {
+  if (nav.hasClass('close')) {
+    $('.burger-btn').removeClass('close');
+    $('.nav-wrapper').fadeToggle(500);
+  }
+});
+
+// よくある質問アコーディオンメニュー部分
+$('.faq__acmenu dd').hide();
+$('.faq__open').show();
+$('.faq__acmenu dt').click(function () {
+  $(this).next().slideToggle();
+});
+
+//制作実務スワイプ部分
+var swiper = new Swiper('.swiper-container', {
+  centeredSlides: true,
+  slidesPerView: 'auto',
+
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+//AOSスライド 部分
+AOS.init({
+  offset: 350,
+  duration: 300,
+  easing: 'ease',
+  delay: 50,
+  once: false,
+  anchorPlacement: 'top-center',
+});
+
+// 指定した場所にスライド移動する部分
+$('.topmove').click(function () {
+  let moveTo = $(this).attr('href');
+  let position = $(moveTo).offset().top - 94;
+  $('html,body').animate({ scrollTop: position }, 700);
+});
+
+// googleフォームに送信する部分
+$(document).ready(function () {
+  $('#form').submit(function (event) {
+    var formData = $('#form').serialize();
+    $.ajax({
+      url: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLScz2CnaP-tRmiDjnHFD8ytuVeUmWdA_-ko2CZQQWm9j0Crr4w/formResponse',
+      data: formData,
+      type: 'POST',
+      dataType: 'xml',
+      statusCode: {
+        0: function () {
+          $('.contact__form-endMessage').slideDown('slow');
+          $('.contact__form-submit').fadeOut('slow');
+          //window.location.href = "thanks.html";
+        },
+      },
+    });
+    event.preventDefault();
+  });
+});
---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+const { src, dest, watch, series, parallel } = require("gulp");
+const Fiber = require("fibers");
+const sass = require("gulp-sass");
+const plumber = require("gulp-plumber");
+const notify = require("gulp-notify");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssdeclsort = require("css-declaration-sorter");
+const gcmq = require("gulp-group-css-media-queries");
+const mode = require("gulp-mode")();
+const browserSync = require("browser-sync");
+const pug = require("gulp-pug");
+const crypto = require("crypto");
+const hash = crypto.randomBytes(8).toString("hex");
+const replace = require("gulp-replace");
+
+sass.compiler = require("sass"); // dart sassを使う
+
+const compileSass = (done) => {
+  const postcssPlugins = [
+    autoprefixer({
+      grid: "autoplace",
+      cascade: false,
+    }),
+    cssdeclsort({ order: "alphabetical" }),
+  ];
+  src("./src/scss/**/*.scss", { sourcemaps: true })
+    .pipe(
+      plumber({ errorHandler: notify.onError("Error: <%= error.message %>") })
+    )
+    .pipe(
+      sass({
+        fiber: Fiber,
+        outputStyle: "expanded",
+      })
+    )
+    .pipe(postcss(postcssPlugins))
+    .pipe(mode.production(gcmq()))
+    .pipe(dest("./dist/css", { sourcemaps: "./sourcemaps" }));
+  done();
+};
+
+const buildServer = (done) => {
+  browserSync.init({
+    port: 8080, // 静的サイト
+    server: { baseDir: "./dist" },
+    // 動的サイト
+    // files: ["./**/*.php"],
+    // proxy: "http://localsite.local/",
+    open: true,
+    watchOptions: {
+      debounceDelay: 1000,
+    },
+  });
+  done();
+};
+
+const browserReload = (done) => {
+  browserSync.reload();
+  done();
+};
+
+const compilePug = (done) => {
+  src(["./src/pug/**/*.pug", "!" + "./src/pug/**/_*.pug"])
+    .pipe(
+      plumber({ errorHandler: notify.onError("Error: <%= error.message %>") })
+    )
+    .pipe(
+      pug({
+        pretty: true,
+      })
+    )
+    .pipe(dest("./dist"));
+  done();
+};
+
+const cacheBusting = (done) => {
+  src("./dist/index.html")
+    .pipe(replace(/\.(js|css)\?ver/g, ".$1?ver=" + hash))
+    .pipe(dest("./dist"));
+  done();
+};
+
+const watchFiles = () => {
+  watch("./src/scss/**/*.scss", series(compileSass, browserReload));
+  watch("./src/pug/**/*.pug", series(compilePug, browserReload));
+};
+
+module.exports = {
+  sass: compileSass,
+  pug: compilePug,
+  cache: cacheBusting,
+  default: parallel(buildServer, watchFiles),
+};
